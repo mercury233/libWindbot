c3660febecc3233275ab64b29a5eb4775f1d4848
mercury233 <me@mercury233.me>
2018-09-02 17:59:53 +0800

    add random bot

--- libWindbot/WindBot.cs
+++ libWindbot/WindBot.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using WindBot.Game;
@@ -14,10 +16,12 @@ namespace WindBot
 
         public static void Test1(string test)
         {
-            WindBotInfo Info = new WindBotInfo();
-            Info.Host = "118.178.111.167";
-            Info.Port = 9999;
-            Info.HostInfo = test;
+            WindBotInfo Info = new WindBotInfo
+            {
+                Host = "118.178.111.167",
+                Port = 9999,
+                HostInfo = test
+            };
             Thread workThread = new Thread(new ParameterizedThreadStart(Run));
             workThread.Start(Info);
             //Windbot.Run(Info);
@@ -35,11 +39,32 @@ namespace WindBot
             AssetPath = assetPath;
             DecksManager.Init();
             NamedCardsManager.Init(databasePath);
+            ReadBots();
+        }
+
+        private static IList<string> ParseArgs(string arg)
+        {
+            return Regex.Split(arg, "(?<=^[^\']*(?:\'[^\']*\'[^\']*)*) (?=(?:[^\']*\'[^\']*\')*[^\']*$)").ToList(); // https://stackoverflow.com/questions/4780728/regex-split-string-preserving-quotes/
         }
 
         public static void RunAndroid(string arg)
         {
-            string[] args = Regex.Split(arg, "(?<=^[^\']*(?:\'[^\']*\'[^\']*)*) (?=(?:[^\']*\'[^\']*\')*[^\']*$)"); // https://stackoverflow.com/questions/4780728/regex-split-string-preserving-quotes/
+            IList<string> args = ParseArgs(arg);
+            Match match = Regex.Match(arg, "Random=(\\w+)");
+            if (match.Success)
+            {
+                //ReadBots();
+                string randomFlag = match.Groups[1].Value;
+                string command = GetRandomBot(randomFlag);
+                if (command != "")
+                {
+                    IList<string> randomArgs = ParseArgs(command);
+                    foreach (string param in randomArgs)
+                    {
+                        args.Add(param);
+                    }
+                }
+            }
             WindBotInfo Info = new WindBotInfo();
             foreach (string param in args)
             {
@@ -57,7 +82,7 @@ namespace WindBot
             workThread.Start(Info);
         }
 
-        public static void Run(object o)
+        private static void Run(object o)
         {
 #if !DEBUG
     try
@@ -93,6 +118,47 @@ namespace WindBot
     }
 #endif
         }
+
+        private static IList<BotInfo> Bots = new List<BotInfo>();
+
+        private static void ReadBots()
+        {
+            StreamReader reader = new StreamReader(new FileStream(Path.Combine(AssetPath, "windbot/bot.conf"), FileMode.Open, FileAccess.Read));
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine().Trim();
+                if (line.Length > 0 && line[0] == '!')
+                {
+                    BotInfo newBot = new BotInfo();
+                    newBot.name = line;
+                    newBot.command = reader.ReadLine().Trim();
+                    newBot.desc = reader.ReadLine().Trim();
+                    line = reader.ReadLine().Trim();
+                    newBot.flags = line.Split(' ');
+                    Bots.Add(newBot);
+                }
+            }
+        }
+
+        private static string GetRandomBot(string flag)
+        {
+            IList<BotInfo> foundBots = Bots.Where(bot => bot.flags.Contains(flag)).ToList();
+            if (foundBots.Count > 0)
+            {
+                Random rand = new Random();
+                BotInfo bot = foundBots[rand.Next(foundBots.Count)];
+                return bot.command;
+            }
+            return "";
+        }
+    }
+
+    public class BotInfo
+    {
+        public string name;
+        public string command;
+        public string desc;
+        public string[] flags;
     }
 
     public class Program
