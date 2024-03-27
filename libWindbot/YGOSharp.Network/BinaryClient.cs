b9c12fad7319895844d4bd3f83a72fdd1cb7de91
mercury233 <me@mercury233.me>
2024-03-27 09:00:20 +0800

    Revert "Remove usage of System.Collections.Generic.Queue"
    This reverts commit cb4dfb9414204e58b2629dac6393d91820c6108f.

--- libWindbot/YGOSharp.Network/BinaryClient.cs
+++ libWindbot/YGOSharp.Network/BinaryClient.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -19,7 +19,7 @@ namespace YGOSharp.Network
         private NetworkClient _client;
 
         private List<byte> _receiveBuffer = new List<byte>();
-        private LinkedList<byte[]> _pendingPackets = new LinkedList<byte[]>();
+        private Queue<byte[]> _pendingPackets = new Queue<byte[]>();
         private byte[] _lengthBuffer = new byte[16];
 
         private int _pendingLength;
@@ -121,8 +121,7 @@ namespace YGOSharp.Network
                 {
                     if (_pendingPackets.Count > 0)
                     {
-                        packet = _pendingPackets.First.Value;
-                        _pendingPackets.RemoveFirst();
+                        packet = _pendingPackets.Dequeue();
                     }
                 }
                 hasReceived = false;
@@ -219,7 +218,7 @@ namespace YGOSharp.Network
 
                 lock (_pendingPackets)
                 {
-                    _pendingPackets.AddLast(packet);
+                    _pendingPackets.Enqueue(packet);
                 }
 
                 return true;
